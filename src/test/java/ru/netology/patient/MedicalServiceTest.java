eb3b740ac484a35cce90a6084ac688d5ed158a1d
Ilnyr Yarkhamov <yarkhamov@mail.ru>
2023-07-17 19:52:45 +0300

    Mockito-tests

--- src/test/java/ru/netology/patient/MedicalServiceTest.java
+++ src/test/java/ru/netology/patient/MedicalServiceTest.java
@@ -17,17 +17,17 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class MedicalServiceTest {
-    HealthInfo health1 = new HealthInfo(new BigDecimal("36.65"), new BloodPressure(120, 80)); //это якобы записно в файле
+    HealthInfo health1 = new HealthInfo(new BigDecimal("36.65"), new BloodPressure(120, 80));
     PatientInfo patient2 = mock(PatientInfo.class);
     PatientInfoFileRepository pIFRep = mock(PatientInfoFileRepository.class);
-    //    SendAlertService aServ = new SendAlertServiceImpl();
+
     SendAlertService aServ1 = mock(SendAlertServiceImpl.class);
     MedicalService mService = new MedicalServiceImpl(pIFRep, aServ1);
     String id1;
-    BloodPressure currentPressure = new BloodPressure(60, 120); //это у реального человека
+    BloodPressure currentPressure = new BloodPressure(60, 120);
     BloodPressure currentPressure2 = new BloodPressure(120, 80);
-    BigDecimal currentTemperature = new BigDecimal("33.0"); //если пониженная температура, то должно быть сообщение
-    BigDecimal currentTemperature2 = new BigDecimal("40.0");//если повышенная, то не должно быть сообщения
+    BigDecimal currentTemperature = new BigDecimal("33.0");
+    BigDecimal currentTemperature2 = new BigDecimal("40.0");
 
     MedicalServiceTest() {
         when(patient2.getId()).thenReturn("14e5fd78-f981-4cb4-ad0c-3bf0aff881db");
@@ -45,7 +45,7 @@ class MedicalServiceTest {
     }
     @Test
     public void testCheckTemperature() {
-        //проверка температуры
+
         mService.checkTemperature(id1, currentTemperature);
         verify(aServ1).send(any());
     }
